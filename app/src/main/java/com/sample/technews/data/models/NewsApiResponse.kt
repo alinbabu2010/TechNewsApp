a2da330d81ce484d11f510265316db3a21e842d2
Alin Babu <alinbabu2010@gmail.com>
2022-08-31 14:24:59 +0530

    Modify API response to add new parameter

--- app/src/main/java/com/sample/technews/data/models/NewsApiResponse.kt
+++ app/src/main/java/com/sample/technews/data/models/NewsApiResponse.kt
@@ -3,5 +3,6 @@ package com.sample.technews.data.models
 data class NewsApiResponse(
     val articles: List<Article>,
     val status: String,
-    val totalResults: Int
+    val totalResults: Int,
+    val message: String
 )
