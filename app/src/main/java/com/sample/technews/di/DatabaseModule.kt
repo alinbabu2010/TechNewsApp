a508050c19e6053f0f23f47224d781a9dfa2d2bd
Alin Babu <alinbabu2010@gmail.com>
2022-09-02 15:46:24 +0530

    Add annotations for database module

--- app/src/main/java/com/sample/technews/di/DatabaseModule.kt
+++ app/src/main/java/com/sample/technews/di/DatabaseModule.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import androidx.room.Room
 import com.sample.technews.data.sources.local.NewsDao
 import com.sample.technews.data.sources.local.NewsDatabase
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 
+@Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
     private const val DB_NAME = "tech_news_db"
