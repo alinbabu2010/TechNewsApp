834a8da38f1b4ec2d79476597473af01e587e45e
Alin Babu <alinbabu2010@gmail.com>
2022-09-02 15:43:57 +0530

    Refactor model class to add default values

--- app/src/main/java/com/sample/technews/data/models/Article.kt
+++ app/src/main/java/com/sample/technews/data/models/Article.kt
@@ -6,12 +6,12 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Article(
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    @Embedded val source: Source,
+    val author: String? = "",
+    val content: String? = "",
+    val description: String? = "",
+    val publishedAt: String? = "",
+    @Embedded val source: Source = Source(),
     @PrimaryKey val title: String,
-    val url: String,
-    val urlToImage: String
+    val url: String? = "",
+    val urlToImage: String? = ""
 )
