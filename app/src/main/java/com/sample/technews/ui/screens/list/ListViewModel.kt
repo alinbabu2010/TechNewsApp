9801a80bb214400a59eb0d7178374c46a6dcf261
Alin Babu <alinbabu2010@gmail.com>
2022-09-02 16:07:31 +0530

    Add view model for news list screen

---
+++ app/src/main/java/com/sample/technews/ui/screens/list/ListViewModel.kt
@@ -0,0 +1,22 @@
+package com.sample.technews.ui.screens.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.sample.technews.data.repositories.NewsRepository
+import com.sample.technews.data.sources.NewsPagingSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListViewModel @Inject constructor(
+    private val newsRepository: NewsRepository
+) : ViewModel() {
+
+    fun getArticles() = Pager(PagingConfig(newsRepository.pageSize)) {
+        NewsPagingSource(newsRepository)
+    }.flow.cachedIn(viewModelScope)
+
+}
