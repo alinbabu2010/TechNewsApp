1d06c99a76cb37f4effcda8e6aefd17fc09eb698
Alin Babu <alinbabu2010@gmail.com>
2022-09-02 15:57:40 +0530

    Add dimension values and common composable

---
+++ app/src/main/java/com/sample/technews/ui/utils/Components.kt
@@ -0,0 +1,60 @@
+package com.sample.technews.ui.utils
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.sample.technews.R
+
+@Composable
+fun LoadingView(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun LoadingItem() {
+    CircularProgressIndicator(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(progressBarSize)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Composable
+fun ErrorItem(
+    message: String,
+    modifier: Modifier = Modifier,
+    onClickRetry: () -> Unit
+) {
+    Row(
+        modifier = modifier.padding(errorItemPadding),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = message,
+            maxLines = 1,
+            modifier = Modifier.weight(1F),
+            style = MaterialTheme.typography.h6,
+            color = Color.Red
+        )
+        OutlinedButton(onClick = onClickRetry) {
+            Text(text = stringResource(R.string.try_again))
+        }
+    }
+}
---
+++ app/src/main/java/com/sample/technews/ui/utils/Dimens.kt
@@ -0,0 +1,10 @@
+package com.sample.technews.ui.utils
+
+import androidx.compose.ui.unit.dp
+
+val newsItemCardCornerSize = 8.dp
+val newsImageHeight = 50.dp
+val newsImageCardHorizontalPadding = 16.dp
+val newsImageCardVerticalPadding = 8.dp
+val progressBarSize = 16.dp
+val errorItemPadding = 16.dp
