3a829f00fb3d4af8091a5958a45c8a03d4bdd6d5
Alin Babu <alinbabu2010@gmail.com>
2022-09-02 09:53:25 +0530

    Modify `Article` data model as database entity

--- app/src/main/java/com/sample/technews/data/models/Article.kt
+++ app/src/main/java/com/sample/technews/data/models/Article.kt
@@ -1,12 +1,16 @@
 package com.sample.technews.data.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Article(
     val author: String,
     val content: String,
     val description: String,
     val publishedAt: String,
     val source: Source,
-    val title: String,
+    @PrimaryKey val title: String,
     val url: String,
     val urlToImage: String
 )
