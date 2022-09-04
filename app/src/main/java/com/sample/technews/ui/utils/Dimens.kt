09d585478b63771a9f8aa4453c44733fdfc67406
Alin Babu <alinbabu2010@gmail.com>
2022-09-04 13:15:03 +0530

    Add dimensions for details screen

--- app/src/main/java/com/sample/technews/ui/utils/Dimens.kt
+++ app/src/main/java/com/sample/technews/ui/utils/Dimens.kt
@@ -1,6 +1,7 @@
 package com.sample.technews.ui.utils
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 val newsItemCardCornerSize = 8.dp
 val newsImageHeight = 150.dp
@@ -9,3 +10,9 @@ val newsImageCardVerticalPadding = 8.dp
 val progressBarSize = 16.dp
 val errorItemPadding = 16.dp
 val retryButtonStartPadding = 2.dp
+
+val detailsColumnPadding = 16.dp
+val detailsImageCornerSize = 4.dp
+val detailsTextTopPadding = 4.dp
+val detailsDescTextTopPadding = 8.dp
+val detailsDescFontSize = 18.sp
