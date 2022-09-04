9312a7ef218d21d11e992df465b3f20468a6dc8c
Alin Babu <alinbabu2010@gmail.com>
2022-09-04 10:17:05 +0530

    Add DI for usecases and map model class for paged data

--- app/src/main/java/com/sample/technews/ui/screens/list/ListViewModel.kt
+++ app/src/main/java/com/sample/technews/ui/screens/list/ListViewModel.kt
@@ -5,15 +5,22 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
+import androidx.paging.map
+import com.sample.technews.data.models.Article
 import com.sample.technews.data.repositories.NewsRepository
 import com.sample.technews.data.paging.NewsPagingSource
+import com.sample.technews.domain.GetArticleWithFormattedDateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class ListViewModel @Inject constructor(
     private val newsRepository: NewsRepository
 ) : ViewModel() {
+
+    @Inject
+    lateinit var getArticleWithFormattedDateUseCase: GetArticleWithFormattedDateUseCase
 
     private val pagingConfig = PagingConfig(
         pageSize = newsRepository.pageSize,
@@ -22,6 +29,8 @@ class ListViewModel @Inject constructor(
 
     fun getArticles() = Pager(pagingConfig) {
         NewsPagingSource(newsRepository)
-    }.flow.cachedIn(viewModelScope)
+    }.flow.map { data ->
+        data.map { getArticleWithFormattedDateUseCase(it) }
+    }
 
 }
