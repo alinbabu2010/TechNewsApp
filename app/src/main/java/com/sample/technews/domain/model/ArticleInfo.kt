e3f38eedfd58537197961928b52d40f3c1532550
Alin Babu <alinbabu2010@gmail.com>
2022-09-04 13:17:02 +0530

    Make `ArticleInfo` as parcelable

--- app/src/main/java/com/sample/technews/domain/model/ArticleInfo.kt
+++ app/src/main/java/com/sample/technews/domain/model/ArticleInfo.kt
@@ -1,5 +1,9 @@
 package com.sample.technews.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ArticleInfo (
     val author: String? = "",
     val content: String? = "",
@@ -8,4 +12,4 @@ data class ArticleInfo (
     val source: String? = "",
     val title: String,
     val urlToImage: String? = ""
-)
+) : Parcelable
